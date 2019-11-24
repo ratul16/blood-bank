d6fab559a740c5e221e7214dd888b23a4652e78a
Ramizoro <you@example.com>
2019-11-24 13:09:00 +0600

    DATA ENTRY: BLOOD_REPOSITORY,B_RECEIPT,HOSPITA

--- blood_bank.sql
+++ blood_bank.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Nov 24, 2019 at 07:03 AM
-=======
--- Generation Time: Nov 24, 2019 at 06:45 AM
->>>>>>> 0b4504590ac6765974f821a31ec91cd33dfc714b
+-- Generation Time: Nov 24, 2019 at 08:06 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -40,6 +36,21 @@ CREATE TABLE `bloodrepository` (
   `Doner_ID` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `bloodrepository`
+--
+
+INSERT INTO `bloodrepository` (`B_Bag_ID`, `Blood_Type`, `Cost`, `S_Quantity`, `Doner_ID`) VALUES
+(10001, 'O+', '500.00', 5, 4151451),
+(10002, 'B+', '500.00', 6, 4151452),
+(10003, 'AB+', '500.00', 3, 4151453),
+(10004, 'O-', '700.00', 7, 4151454),
+(10005, 'O+', '700.00', 5, 4151455),
+(10006, 'A-', '700.00', 4, 4151456),
+(10007, 'B-', '700.00', 3, 4151457),
+(10008, 'O+', '500.00', 3, 4151458),
+(10009, 'A+', '500.00', 5, 4151459);
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +64,26 @@ CREATE TABLE `breceipt` (
   `Blood_Type` varchar(3) NOT NULL,
   `R_Quantity` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `breceipt`
+--
+
+INSERT INTO `breceipt` (`Buyers_ID`, `Order_ID`, `Order_date`, `Blood_Type`, `R_Quantity`) VALUES
+(2190001, 1518001, '0000-00-00', 'B+', 3),
+(2190002, 1518003, '0000-00-00', 'O+', 2),
+(2190003, 1518002, '0000-00-00', 'A+', 3),
+(2190004, 1518005, '0000-00-00', 'O-', 4),
+(2190005, 1518003, '0000-00-00', 'O+', 3),
+(2190006, 1518003, '0000-00-00', 'O+', 2),
+(2190007, 1518007, '0000-00-00', 'O+', 3),
+(2190001, 1518001, '0000-00-00', 'B+', 3),
+(2190002, 1518003, '0000-00-00', 'O+', 2),
+(2190003, 1518002, '0000-00-00', 'A+', 3),
+(2190004, 1518005, '0000-00-00', 'O-', 4),
+(2190005, 1518003, '0000-00-00', 'O+', 3),
+(2190006, 1518003, '0000-00-00', 'O+', 2),
+(2190007, 1518007, '0000-00-00', 'O+', 3);
 
 -- --------------------------------------------------------
 
@@ -69,7 +100,6 @@ CREATE TABLE `donar` (
   `Gender` varchar(6) NOT NULL,
   `Last_Donate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-<<<<<<< HEAD
 
 -- --------------------------------------------------------
 
@@ -81,11 +111,20 @@ CREATE TABLE `hospital` (
   `Buyers_ID` int(7) NOT NULL,
   `Name` varchar(20) NOT NULL,
   `Branch` varchar(20) NOT NULL,
-  `Phone` int(13) NOT NULL,
+  `Phone` varchar(13) NOT NULL,
   `Address` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-=======
->>>>>>> 0b4504590ac6765974f821a31ec91cd33dfc714b
+
+--
+-- Dumping data for table `hospital`
+--
+
+INSERT INTO `hospital` (`Buyers_ID`, `Name`, `Branch`, `Phone`, `Address`) VALUES
+(2138001, 'Ad-Din Women\'s Medic', 'Mogbazar', '02-9353391', '2 Bara Maghbazar, Ou'),
+(2138002, 'Holy Family Red Cres', 'Eskaton', '02-48311721', '1 Eskaton Garden Rd,'),
+(2138003, 'Rushmono Specialized', 'Mogbazar', '01918-378372', 'Mogbazar Plaza, 209 '),
+(2138004, 'S.P.R.C & Neurology ', 'Eskaton', '02-9339089', '135 New Eskaton Road'),
+(2138005, 'Monowara Hospital', 'Siddeswari', ' 02-48318135', '54 Siddheswari Rd, D');
 
 -- --------------------------------------------------------
 
