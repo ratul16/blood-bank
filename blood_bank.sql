6deadfc2c31213d248fc5bfb1dede9e8af92e9aa
rukia1423@gmail.com <you@example.com>
2019-11-24 09:06:07 +0600

    SQL FILE 1st DRAFT

---
+++ blood_bank.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2019 at 03:47 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blood_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloodrepository`
+--
+
+CREATE TABLE `bloodrepository` (
+  `B_Bag_ID` int(5) NOT NULL,
+  `Blood_Type` varchar(3) NOT NULL,
+  `Cost` decimal(5,2) NOT NULL,
+  `S_Quantity` int(11) NOT NULL,
+  `Doner_ID` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doner`
+--
+
+CREATE TABLE `doner` (
+  `Doner_ID` int(7) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `DOB` date DEFAULT NULL,
+  `Gender` varchar(6) NOT NULL,
+  `Last_Donated` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal`
+--
+
+CREATE TABLE `personal` (
+  `Buyers_ID` int(7) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `Blood_Type` varchar(3) NOT NULL,
+  `Phone` int(13) NOT NULL,
+  `Address` varchar(20) DEFAULT NULL,
+  `DOB` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `doner`
+--
+ALTER TABLE `doner`
+  ADD PRIMARY KEY (`Doner_ID`),
+  ADD UNIQUE KEY `Doner_ID` (`Doner_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
