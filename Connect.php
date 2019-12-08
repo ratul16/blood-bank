6308b19d76343fd9a6ea53a51c23379a7a261239
SadiaSheikh <57584128+SadiaSheikh@users.noreply.github.com>
2019-12-08 14:54:45 +0600

    Connection Code Added

---
+++ Connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$db['hostname'] = "localhost";
+$db['user'] = "root";
+$db['password']="";
+$db ['database'] = "blood_bank";
+
+$con = mysqli_connect ($db['hostname'],$db['user'],$db['password'],$db['database']);
+
+if (mysqli_connect_errno()) {
+   echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+}
+?>
