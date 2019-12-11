7b206a8aaf97e8a5cb6715e098b3b1301d39b1bf
diya <ishratdiya222@gmail.com>
2019-12-11 13:02:59 +0600

    Connect.php checked and slightly modified. No major changes

--- Connect.php
+++ Connect.php
@@ -10,4 +10,7 @@ $con = mysqli_connect ($db['hostname'],$db['user'],$db['password'],$db['database
 if (mysqli_connect_errno()) {
    echo "Failed to connect to MYSQL: " . mysqli_connect_error();
 }
+else{
+    //echo "connected";
+}
 ?>
